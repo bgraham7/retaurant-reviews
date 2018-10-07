835adfadfd4cd26008c3d7f95b458c79e3a57e8b
bgraham <bgraham626@gmail.com>
2018-10-07 12:25:06 -0400

    Image Alts

--- js/main.js
+++ js/main.js
@@ -160,6 +160,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = restaurant.name + " Image";
   li.append(image);
 
   const name = document.createElement('h1');
